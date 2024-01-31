cbf665c1213fa54f571b0166024432ea5bb6b733
HiranSasitha <hiransasitha123@gamil.com>
2024-01-31 11:31:33 +0530

    completed orderplace

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Checkout = () => {
@@ -7,36 +7,37 @@ const Checkout = () => {
   const [itemsWithQuantity, setItemsWithQuantity] = useState(        // create another item array with new quantity
     storedItems.map((item) => ({ ...item, quantity: 1 }))
   );
-  const[total,setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
   const [Category, setCategory] = useState(null);
   const [adjustable, setAdjustable] = useState(null);
+  const navigate = useNavigate();
 
-  
+
 
   let calTotal = 0;
   itemsWithQuantity.forEach((item) => {
     calTotal += item.sellingPrice * item.quantity;
   });
-  
- useEffect(()=>{
-  setTotal(calTotal);
- },[calTotal])
 
- useEffect(() => {
-  getAllCategory();
-  getAdjustable();
-  
-}, [])
+  useEffect(() => {
+    setTotal(calTotal);
+  }, [calTotal])
 
-const getAllCategory = async () => {
-  const response = await axios.get("http://localhost:8080/category");
-  setCategory(response.data);
-}
-const getAdjustable = async () => {
-  const response = await axios.get("http://localhost:8080/adjusbill");
-  setAdjustable(response.data);
-  console.log(response.data);
-}
+  useEffect(() => {
+    getAllCategory();
+    getAdjustable();
+
+  }, [])
+
+  const getAllCategory = async () => {
+    const response = await axios.get("http://localhost:8080/category");
+    setCategory(response.data);
+  }
+  const getAdjustable = async () => {
+    const response = await axios.get("http://localhost:8080/adjusbill");
+    setAdjustable(response.data);
+    console.log(response.data);
+  }
 
 
 
@@ -74,12 +75,41 @@ const getAdjustable = async () => {
     );
   };
 
- 
-  
+  const placeOrder = async () => {
+    try {
+      const orderItemDtos = itemsWithQuantity.map((item) => ({
+        itemId: item.id,
+        qty: item.quantity,
+      }));
+
+      const userId = 2;
+
+      const data = {
+        "userId": userId,
+        "orderItemDtos": orderItemDtos
+      };
+
+      const response = await axios.post("http://localhost:8080/order", data);
+      console.log(response);
+
+      if (response.status === 200) {
+        clearCart();
+        setTotal(0);
+        alert("Successful Place Order");
+        navigate("/");
+
+      }
+
+
+    } catch (error) {
+      alert("Fail Place Order");
+    }
+  }
+
 
   return (
     <>
-    <nav class="n1 navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <nav class="n1 navbar bg-dark border-bottom border-body" data-bs-theme="dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src='/image/logo.png' alt="Logo" width="300" height="100" class="d-inline-block align-text-top" />
@@ -157,7 +187,13 @@ const getAdjustable = async () => {
         </div>
       </div>
 
-      <h2>Checkout</h2>
+      <div className="d-flex justify-content-between align-items-center mb-3 me-5 ms-3">
+        <h2 className='t1'>My Cart</h2>
+        {itemsWithQuantity.length > 0 &&
+          <button className="btn btn-warning me-5" onClick={clearCart}>
+            Clear Cart
+          </button>}
+      </div>
       <table className="table table-bordered">
 
         <tbody>
@@ -185,7 +221,7 @@ const getAdjustable = async () => {
                     +
                   </button>
                 </div>
-               
+
               </td >
               <td className="centered-td">Rs.{item.sellingPrice * item.quantity}</td>
               <td className="centered-td">
@@ -198,129 +234,175 @@ const getAdjustable = async () => {
               </td>
             </tr>
           ))}
-          {itemsWithQuantity.length > 0&&
-          <tr class="table-danger" >
-          <td className="centered-td"colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-            <td className="centered-td" colSpan="2">TOTAL</td>
-            <td  className="centered-td fw-bold" colSpan="2"style={{ borderLeft: 'none' }}>Rs.{total}</td>
-          </tr>
-}
+          {itemsWithQuantity.length > 0 &&
+            <tr class="table-danger" >
+              <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+              <td className="centered-td" colSpan="2">TOTAL</td>
+              <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>Rs.{total}</td>
+            </tr>
+          }
 
-{itemsWithQuantity.length > 0&&
-          <tr class="table-danger" >
-          <td className="centered-td"colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-            <td className="centered-td" colSpan="2">Tax</td>
-            {adjustable && adjustable.map((adj)=>
-             <td  className="centered-td fw-bold" colSpan="2"style={{ borderLeft: 'none' }}>Rs.{(total*adj.tax)/100} ({adj.tax}%)</td>
-            
+          {itemsWithQuantity.length > 0 &&
+            <tr class="table-danger" >
+              <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+              <td className="centered-td" colSpan="2">Tax</td>
+              {adjustable && adjustable.map((adj) =>
+                <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>Rs.{(total * adj.tax) / 100} ({adj.tax}%)</td>
+
+              )
+              }
+
+            </tr>
+          }
+
+          {itemsWithQuantity.length == 0 &&
+            <div className=" t3 container-center mt-5 mb-5">
+              <h3 className="t2">Not Item In Cart</h3>
+            </div>
+          }
+
+          {adjustable && itemsWithQuantity.length > 0 &&
+            adjustable.map((adj) =>
+              total < adj.upToAmount && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Sub Total</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    Rs.{total + ((total * adj.tax) / 100)}
+                  </td>
+                </tr>
+              )
             )
-            }
-           
-          </tr>
-}
+          }
 
-{itemsWithQuantity.length == 0 &&
-<h5>No Item In Cart</h5>
-}
+          {adjustable &&
+            adjustable.map((adj) =>
+              total > adj.upToAmount && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Discount</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    {adj.discount}%
+                  </td>
+                </tr>
+              )
+            )
+          }
 
-{adjustable && itemsWithQuantity.length >0 &&
-  adjustable.map((adj) =>
-    total < adj.upToAmount && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Sub Total</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-          Rs.{total+((total*adj.tax)/100)}
-        </td>
-      </tr>
-    )
-  )
-}
+          {adjustable &&
+            adjustable.map((adj) =>
+              total > adj.upToAmount && ((total * adj.discount) / 100) > adj.maxDiscountPrice && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Discount Price</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    Rs. {adj.maxDiscountPrice}(Max discount Price)
+                  </td>
+                </tr>
 
-{adjustable &&
-  adjustable.map((adj) =>
-    total > adj.upToAmount && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Discount</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-          {adj.discount}%
-        </td>
-      </tr>
-    )
-  )
-}
 
-{adjustable &&
-  adjustable.map((adj) =>
-    total > adj.upToAmount && ((total*adj.discount)/100)> adj.maxDiscountPrice && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Discount Price</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-         Rs. {adj.maxDiscountPrice}(Max discount Price)
-        </td>
-      </tr>
-    
-      
-    )
-  )
-}
+              )
+            )
+          }
 
-{adjustable &&
-  adjustable.map((adj) =>
-    total > adj.upToAmount && ((total*adj.discount)/100)> adj.maxDiscountPrice && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Sub Total</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-         Rs. {total+((total*adj.tax)/100)-adj.maxDiscountPrice}
-        </td>
-      </tr>
-    
-      
-    )
-  )
-}
+          {adjustable &&
+            adjustable.map((adj) =>
+              total > adj.upToAmount && ((total * adj.discount) / 100) > adj.maxDiscountPrice && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Sub Total</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    Rs. {total + ((total * adj.tax) / 100) - adj.maxDiscountPrice}
+                  </td>
+                </tr>
 
-{adjustable &&
-  adjustable.map((adj) =>
-    total > adj.upToAmount && ((total*adj.discount)/100)< adj.maxDiscountPrice && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Discount Price</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-         Rs. {(total*adj.discount)/100}
-        </td>
-      </tr>
-    
-      
-    )
-  )
-}
 
-{adjustable &&
-  adjustable.map((adj) =>
-    total > adj.upToAmount && ((total*adj.discount)/100)< adj.maxDiscountPrice && (
-      <tr className="table-danger" key={adj.id}>
-        <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
-        <td className="centered-td" colSpan="2">Sub Total</td>
-        <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
-         Rs. {total+((total*adj.tax)/100)-((total*adj.discount)/100)}
-        </td>
-      </tr>
-    
-      
-    )
-  )
-}
+              )
+            )
+          }
+
+          {adjustable &&
+            adjustable.map((adj) =>
+              total > adj.upToAmount && ((total * adj.discount) / 100) < adj.maxDiscountPrice && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Discount Price</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    Rs. {(total * adj.discount) / 100}
+                  </td>
+                </tr>
+
+
+              )
+            )
+          }
+
+          {adjustable &&
+            adjustable.map((adj) =>
+              total > adj.upToAmount && ((total * adj.discount) / 100) < adj.maxDiscountPrice && (
+                <tr className="table-danger" key={adj.id}>
+                  <td className="centered-td" colSpan="3" style={{ borderRight: 'none', backgroundColor: 'transparent' }}></td>
+                  <td className="centered-td" colSpan="2">Sub Total</td>
+                  <td className="centered-td fw-bold" colSpan="2" style={{ borderLeft: 'none' }}>
+                    Rs. {total + ((total * adj.tax) / 100) - ((total * adj.discount) / 100)}
+                  </td>
+                </tr>
+
+
+              )
+            )
+          }
 
         </tbody>
       </table>
-      <Link to="/">HOME</Link>
-      <button className="btn btn-warning" onClick={clearCart}>
-        Clear Cart
-      </button>
+      <div className="t4 d-flex justify-content-end me-5 mt-3 mb-5">
+        {itemsWithQuantity.length > 0 &&
+
+          <button type="button" className="btn btn-success" onClick={placeOrder}>Place Order</button>
+        }
+
+      </div>
+      <div className='d1'>
+        <div className="container-fluid">
+
+          <div className='  row ms-2'>
+
+            <div className='c1 col-12 col-md-3  mt-4 '>
+              <p><img src="/image/delivery.png" width="50" height="35" class="d-inline-block align-text-top me-2" />Island delivery</p>
+            </div>
+
+            <div className='c1 col-12 col-md-3 mt-4'>
+              <p><img src="/image/payment.png" width="50" height="35" class="d-inline-block align-text-top me-2" />Secure Payments</p>
+            </div>
+
+            <div className='c1 col-12 col-md-3 mt-4'>
+              <p><img src="/image/price.png" width="50" height="35" class="d-inline-block align-text-top me-2" />Best Price</p>
+            </div>
+
+            <div className='c1 col-12 col-md-3 mt-4'>
+              <p><img src="/image/discount.png" width="50" height="35" class="d-inline-block align-text-top me-2" />Best Discount</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className='d2 row'>
+          <div className=' text-center'>
+            <img src='/image/logo.png' alt="Logo" width="200" height="100" class="d-inline-block align-text-top" />
+          </div>
+        </div>
+        <div className='d3 row'>
+          <div className=' text-center'>
+            <p className='mt-2'>Copyright © 2024- HS Bookshop - All Rights Reserved. Concept, Design & Development By Hiran</p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
     </>
   );
 };
--- src/ItemByCategory.js
+++ src/ItemByCategory.js
@@ -139,8 +139,6 @@ const ItemByCategory = () => {
 
             </div>)}
 
-
-
           </div>
         </div>
       </div>
