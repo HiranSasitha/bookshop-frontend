1ccbf40928d911528d61f6b38f5248b029ecc3c0
HiranSasitha <hiransasitha123@gamil.com>
2024-02-01 18:37:51 +0530

    completed manage discount

--- src/admin/ManageDiscount.js
+++ src/admin/ManageDiscount.js
@@ -1,6 +1,92 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { event } from "jquery";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const ManageDiscount = ()=> {
+    const[discount,setDiscount] = useState("");
+    const[upToAmount,setUpToAmount] = useState("");
+    const[tax,setTax] = useState("");
+    const[maxDiscountPrice,setMaxDiscountPrice] = useState("");
+    const [adjustable, setAdjustable] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+       
+        getAdjustable();
+        
+      }, [])
+      useEffect(() => {
+        setInput();
+    }, [adjustable]);
+
+    const setInput = ()=>{
+        {adjustable && adjustable.map((adj)=>
+           setDiscount(adj.discount)   
+            )}
+            {adjustable && adjustable.map((adj)=>
+                setUpToAmount(adj.upToAmount)   
+                 )}
+
+                 {adjustable && adjustable.map((adj)=>
+                    setTax(adj.tax)   
+                     )}
+
+                     {adjustable && adjustable.map((adj)=>
+                        setMaxDiscountPrice(adj.maxDiscountPrice)   
+                         )}
+    }
+
+
+
+    const getAdjustable = async () => {
+        const response = await axios.get("http://localhost:8080/adjusbill");
+        setAdjustable(response.data);
+       
+      }
+
+    const handleDiscount = (event)=>{
+        const value = event.target.value;
+        if (/^\d*\.?\d*$/.test(value)) {
+            setDiscount(value);
+    }
+}
+    const handleUptoAmunt = (event)=>{
+        const value = event.target.value;
+        if (/^\d*\.?\d*$/.test(value)) {
+            setUpToAmount(value);
+    }
+}
+    const handleTax = (event)=>{
+        const value = event.target.value;
+        if (/^\d*\.?\d*$/.test(value)) {
+            setTax(value);
+    }
+}
+    const handleMaxDiscountPrice = (event)=>{
+        const value = event.target.value;
+        if (/^\d*\.?\d*$/.test(value)) {
+            setMaxDiscountPrice(value);
+    }
+}
+
+const upadeMangeDiscount = async ()=>{
+  
+    const data = {
+        "discount": discount,
+        "upToAmount": upToAmount,
+        "tax": tax,  
+        "maxDiscountPrice": maxDiscountPrice
+    }
+
+    const response = await axios.put(`http://localhost:8080/adjusbill/${1}/update`,data);
+    if (response.status === 200) {
+        alert("Success Update");
+        navigate("/admin");
+
+    }
+}
+
     return(
         <>
          <nav class="n1 navbar bg-dark border-bottom border-body" data-bs-theme="dark">
@@ -68,8 +154,48 @@ const ManageDiscount = ()=> {
             </nav>
 
             <div className="text-center my-4"> 
-      <h2 className="k1">Order Details</h2>
+      <h2 className="k1">Manage Discount</h2>
     </div>
+
+    <div class="card mx-5 mb-5 border border-secondary rounded-3">
+                <h5 class="card-header card-header text-center fw-bold bg-secondary text-white">Manage</h5>
+                <div class="card-body">
+                    <form onSubmit={upadeMangeDiscount}>
+                        <label className="form-label">
+                            Discount(%)
+                        </label>
+                        <div className="form-group mb-3">
+                            <input type="text" className="form-control" onChange={handleDiscount} value={discount} placeholder="Discount" required />
+                        </div>
+                        
+                        <label className="form-label">
+                            Up to Amount(Rs)
+                        </label>
+                        <div className="form-group mb-3">
+                            <input type="text" className="form-control" onChange={handleUptoAmunt} value={upToAmount} placeholder="Up to Amount" required />
+                        </div>
+
+                        <label className="form-label">
+                            Tax(%) 
+                        </label>
+                        <div className="form-group mb-3">
+                            <input type="text" className="form-control" onChange={handleTax} value={tax} placeholder="Tax" required />
+                        </div>
+
+                        <label className="form-label">
+                            Max Discount(Rs)
+                        </label>
+                        <div className="form-group mb-3">
+                            <input type="text" className="form-control" onChange={handleMaxDiscountPrice} value={maxDiscountPrice} placeholder="Max Discount" required />
+                        </div>
+                        
+
+                        <button type="submit" className="btn btn-primary">Update</button>
+                    </form>
+
+                </div>
+            </div>
+
         </>
     )
 }
